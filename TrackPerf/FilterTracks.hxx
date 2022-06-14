317402ee8a2688fb530e1ae6f782cadac562412f
njbruhwiler <51885369+njbruhwiler@users.noreply.github.com>
2022-06-14 10:41:11 -0600

    Update FilterTracks.hxx

--- TrackPerf/FilterTracks.hxx
+++ TrackPerf/FilterTracks.hxx
@@ -1,1 +1,50 @@
+#pragma once
+
+#include <marlin/Processor.h>
+
+
+namespace TrackPerf
+{
+}
+
+class FilterTracks : public marlin::Processor
+{
+public:
+   virtual Processor* newProcessor() { return new FilterTracks ; }
+
+   FilterTracks(const FilterTracks &) = delete ;
+   FilterTracks& operator =(const FilterTracks &) = delete ;
+   FilterTracks() ;
+
+   /** Called at the begin of the job before anything is read.
+    * Use to initialize the processor, e.g. book histograms.
+    */
+   virtual void init() ;
+
+   /** Called for every run.
+    */
+   virtual void processRunHeader( LCRunHeader* run ) ;
+
+   /** Called for every event - the working horse.
+    */
+   virtual void processEvent(LCEvent* evt) ;
+
+
+   /** Called after data processing for clean up.
+    */
+   virtual void end() ;
+
+private:
+   //! Input track collection
+   std::string _InTrackCollection {};
+
+   //! Output track collection
+   std::string _OutTrackCollection {};
+
+   //! Cut off for number of hits
+   int _NHits = 5;
+   
+   //! Cut off for momentum
+   float _MinPt = 1.0;
+};
 
