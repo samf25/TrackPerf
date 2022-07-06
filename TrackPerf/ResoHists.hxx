bb39f581878a9156c8a72ddefb8014213d7fddb0
Natalie Bruhwiler <natalie.bruhwiler@gmail.com>
2022-07-06 15:07:47 -0700

    Add ResoHists

---
+++ TrackPerf/ResoHists.hxx
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <TH1.h>
+#include <TH2.h>
+#include <TVector2.h>
+
+namespace EVENT
+{
+  class Track;
+  class MCParticle;
+}
+
+namespace TrackPerf
+{
+  //! Histograms for reconstructed tracks
+  class ResoHists
+  {
+  public:
+    ResoHists(const ResoHists &) = delete ;
+    ResoHists& operator =(const ResoHists &) = delete ;
+
+    //! Initialize empty histograms
+    ResoHists() ;
+
+    // Fill histograms with a single track
+    void fill(const EVENT::Track* track, const EVENT::MCParticle* particle);
+
+  private:
+    //! magnetic field to use for curvature -> pT conversion
+    float _Bz=3.57;
+
+    //! Reconstructed track pT
+    TH2* h_track_truth_pt;
+    TH1* h_reso_pt;
+    TH1* h_reso_lambda;
+    TH1* h_reso_phi;
+  };
+}
