88b9b204202e7518e4a67b126474e3fd3322b5c3
Natalie Bruhwiler <natalie.bruhwiler@gmail.com>
2022-07-07 08:45:25 -0700

    Add ResoHists

--- src/ResoHists.cxx
+++ src/ResoHists.cxx
@@ -8,9 +8,9 @@ using namespace TrackPerf;
 ResoHists::ResoHists()
 {
   h_track_truth_pt       = new TH2F("track_vs_truth_pt" , ";Track p_{T} [GeV];Truth p_{T} [GeV]" , 100, 0, 10, 100, 0, 10  );
-  h_reso_pt              = new TH1F("reso_pt"       , ";Resolution;Tracks"       , 100, -1, 1);
-  h_reso_lambda          = new TH1F("reso_lambda"   , ";Resolution;Tracks"       , 100, -1, 1);
-  h_reso_phi             = new TH1F("reso_phi"      , ";Resolution;Tracks"       , 100, -1, 1);
+  h_reso_pt              = new TH1F("reso_pt"       , ";(truth pt - track pt)/truth pt;Tracks", 100, -1, 1);
+  h_reso_lambda          = new TH1F("reso_lambda"   , ";truth lambda - track lambda;Tracks"   , 100, -1, 1);
+  h_reso_phi             = new TH1F("reso_phi"      , ";truth phi - track phi;Tracks"         , 100, -1, 1);
 }
 
 void ResoHists::fill(const EVENT::Track* track, const EVENT::MCParticle* particle)
@@ -23,6 +23,9 @@ void ResoHists::fill(const EVENT::Track* track, const EVENT::MCParticle* particl
   double truth_lambda=std::atan2(mom[2],truth_pt);
   double truth_phi=std::atan2(mom[1],mom[0]);
   double delta_phi=TVector2::Phi_mpi_pi(truth_phi-track->getPhi());
+  std::cout << "Truth phi: " << truth_phi << std::endl;
+  std::cout << "Track phi: " << track->getPhi() << std::endl;
+  std::cout << "Delta phi: " << delta_phi << std::endl;
 
   h_track_truth_pt  ->Fill(track_pt,truth_pt);
   h_reso_pt         ->Fill((truth_pt-track_pt)/truth_pt);
