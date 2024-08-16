d1d67b279b33663a5a984c4f616d65c6f76ecfc5
Samuel Ferraro <sferrar2@login29.chn.perlmutter.nersc.gov>
2024-08-16 08:12:11 -0700

    Removed Hard MCP Reconstructability checks

--- src/EfficiencyHists.cxx
+++ src/EfficiencyHists.cxx
@@ -11,9 +11,9 @@ EfficiencyHists::EfficiencyHists(bool effi) {
      h_efftheta = new TEfficiency("theta_vs_eff", ";Truth theta; Efficiency", 50, -4, 4);
   } else {
     h_effpt = new TEfficiency("fake_pt_vs_eff",
-      "Fake Track Rate vs pT;Reconstructed pT [GeV]; Efficiency", 50, 0, 12);
+      "Fake Track Rate vs pT;Reconstructed pT [GeV]; Fake Rate", 50, 0, 12);
     h_efftheta = new TEfficiency("fake_theta_vs_eff",
-      "Fake Track Rate vs Theta;Reconstructed theta; Efficiency", 50, -4, 4);
+      "Fake Track Rate vs Theta;Reconstructed theta; Fake Rate", 50, -4, 4);
   }
 }
 
@@ -21,14 +21,14 @@ void EfficiencyHists::fillTrack(const EVENT::Track* track, bool passed) {
   float pt = fabs(0.3 * _Bz / track->getOmega() / 1000);
   float theta = TMath::Pi() - std::atan(track->getTanLambda());
 
-  if (fabs(theta) < 2*std::atan(std::exp(-2))) h_effpt->Fill(passed, pt);
-  if (pt > 0.5) h_efftheta->Fill(passed, theta);
+  h_effpt->Fill(passed, pt);
+  h_efftheta->Fill(passed, theta);
 }
 
 void EfficiencyHists::fillMC(const EVENT::MCParticle* particle, bool passed) {
   const double* mom = particle->getMomentum();
   double pt = std::sqrt(std::pow(mom[0], 2) + std::pow(mom[1], 2));
   double theta = std::atanh(mom[2] / pt);
-  if (pt > 0.5) h_efftheta->Fill(passed, theta);
-  if (fabs(theta) < 2*std::atan(std::exp(-2))) h_effpt->Fill(passed, pt);
+  h_efftheta->Fill(passed, theta);
+  h_effpt->Fill(passed, pt);
 }
