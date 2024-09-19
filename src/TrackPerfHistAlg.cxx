8c04d181a1a3465b1aa0f3fa5af8b880895b2088
samf25 <samuel_ferraro@brown.edu>
2024-09-19 19:34:49 -0400

    issue search

--- src/TrackPerfHistAlg.cxx
+++ src/TrackPerfHistAlg.cxx
@@ -77,6 +77,7 @@ void TrackPerfHistAlg::operator()(
 
 	// Tracks
 	std::vector<edm4hep::Track> trkSet;
+	log << MSG::WARNING << "Track Collection Size: " << tracks.size() << endmsg;
 	for (const auto& trk : tracks) {
 		trkSet.push_back(trk);
 		m_allTracks->fill(&trk);
